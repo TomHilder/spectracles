d1497f954587fe98f240b842d6d35fa04ef7a402
Tom Hilder <tom.hilder.dlhp@gmail.com>
2025-04-29 12:08:11 +1000

    updated __init__ to include all user-facing stuff that is implemented

--- src/modelling_lib/__init__.py
+++ src/modelling_lib/__init__.py
@@ -1,4 +1,18 @@
-from .spatial import FourierGP
-from .spectral import Gaussian
+from .data import SpatialData
+from .kernels import Matern12, Matern32, Matern52, SquaredExponential
+from .leaf_sharing import build_model
+from .spatial import FourierGP, PerSpaxel
+from .spectral import Constant, Gaussian
 
-__all__ = ["FourierGP", "Gaussian"]
+__all__ = [
+    "FourierGP",
+    "Gaussian",
+    "SpatialData",
+    "PerSpaxel",
+    "Constant",
+    "Matern12",
+    "Matern32",
+    "Matern52",
+    "SquaredExponential",
+    "build_model",
+]
