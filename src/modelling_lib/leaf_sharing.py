55ee847224b482e433f0ed8c8f6ba7bfbbcc1729
Tom Hilder <tom.hilder.dlhp@gmail.com>
2025-05-01 17:26:04 +1000

    made parameters and sharing stuff more sensible

--- src/modelling_lib/leaf_sharing.py
+++ src/modelling_lib/leaf_sharing.py
@@ -1,12 +1,12 @@
 from typing import Any, Callable, Dict, Tuple
 
-from equinox import Module, field, filter, tree_at
+from equinox import Module, field, filter, is_inexact_array, tree_at
 from jax.tree import leaves_with_path
 from jax.tree_util import tree_map
 from jaxlib.xla_extension.pytree import DictKey, GetAttrKey, SequenceKey
 from jaxtyping import Array, PyTree
 
-from .parameter import Param
+from .parameter import Parameter
 
 LeafKey = SequenceKey | DictKey | GetAttrKey
 LeafPath = Tuple[LeafKey, ...]
@@ -35,11 +35,15 @@ def use_paths_get_leaves(tree: PyTree, paths: list[LeafPath]) -> list[Any]:
 
 
 def get_duplicated_leaves(tree: PyTree) -> Tuple[list[int], list[LeafPath], dict[int, LeafPath]]:
-    # Filter out leaves that are not Param
+    # Filter out leaves that are not Parameter
     filter_spec = tree_map(
-        lambda x: isinstance(x, Param), tree, is_leaf=lambda x: isinstance(x, Param)
+        lambda x: isinstance(x, Parameter), tree, is_leaf=lambda x: isinstance(x, Parameter)
     )
     filtered_tree = filter(tree, filter_spec=filter_spec)
+    # Filter out leaves that are not inexact arrays (avoids Parameter class's other attributes)
+    filtered_tree = filter(
+        filtered_tree, filter_spec=tree_map(lambda x: is_inexact_array(x), filtered_tree)
+    )
     leaves = leaves_with_path(filtered_tree)
     # Create a dictionary to keep track of the parent leaves
     parent_leaf_paths: dict[int, LeafPath] = dict()
@@ -60,6 +64,16 @@ def get_duplicated_leaves(tree: PyTree) -> Tuple[list[int], list[LeafPath], dict
     return dupl_leaf_ids, dupl_leaf_paths, parent_leaf_paths
 
 
+class Shared:
+    """A sentinel object used to indicate a parameter is shared."""
+
+    def __repr__(self):
+        return "shared"
+
+    def __str__(self):
+        return "shared"
+
+
 class ShareModule(Module):
     model: Module
 
@@ -75,7 +89,7 @@ class ShareModule(Module):
             self._parent_leaf_paths,
         ) = get_duplicated_leaves(model)
         # Remove leaves that are coupled to other leaves
-        self.model = tree_at(self._where, model, replace_fn=lambda _: 0)
+        self.model = tree_at(self._where, model, replace_fn=lambda _: Shared())
 
     def __call__(self, *args, **kwargs):
         # Replace nodes specified by `where` with the nodes specified by `get`
--- src/modelling_lib/parameter.py
+++ src/modelling_lib/parameter.py
@@ -3,38 +3,30 @@ from equinox import Module
 from jaxtyping import Array
 
 
-class Param(Module):
-    """A varied parameter that is trainable. This is just for the purpose of internal tree surgery required when fitting the model."""
+class Parameter(Module):
+    """A parameter in a statistical model, which may be fixed or free."""
 
     value: Array
+    fixed: bool
 
-
-class Parameter:
-    """Factory class for instantiating parameters and avoiding the user screwing it up."""
-
-    def __new__(
-        self, dims: int | tuple = None, initial: Array = None, fixed: bool = False
-    ) -> Param | Array:
-        # If user provided nothing assume scalar parameter
+    def __init__(self, dims: int | tuple = None, initial: Array = None, fixed: bool = False):
         if dims is None and initial is None:
             shape = (1,)
-        # If user provided initial that doesn't match the shape
         elif initial is not None and dims is not None and dims != initial.shape:
             raise ValueError("The shape of initial must match dims.")
         else:
             shape = dims
 
-        # Return either zeros
         if initial is None:
             param_vals = jnp.zeros(shape)
-        # Or a copy of their initial values
         else:
-            param_vals = self.to_float_array(initial).copy()
+            param_vals = self._to_float_array(initial).copy()
 
-        return Param(param_vals) if not fixed else param_vals
+        self.value = param_vals
+        self.fixed = fixed
 
     @staticmethod
-    def to_float_array(x):
-        if jnp.isscalar(x):  # catches int, float, etc.
+    def _to_float_array(x):
+        if jnp.isscalar(x):
             return jnp.array([float(x)])
         return jnp.asarray(x, dtype=float)
