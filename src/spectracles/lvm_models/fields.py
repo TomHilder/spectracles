78b6d36510fe1d23201e0dc3322ee0ee2954820f
Tom Hilder <tom.hilder.dlhp@gmail.com>
2026-01-20 13:42:48 +1100

    Improve model building by making general field types that handle common cases

---
+++ src/spectracles/lvm_models/fields.py
@@ -0,0 +1,100 @@
+"""fields.py - Spatial model field components for LVM. Probably move to spectracles.model.spatial later."""
+
+import jax.numpy as jnp
+from jaxtyping import Array
+
+from spectracles import FourierGP, Kernel, PerSpaxel, SpatialData, SpatialModel, l_bounded
+
+
+class GPField(SpatialModel):
+    """A spatial field model represented by a Gaussian Process."""
+
+    gp: FourierGP  # The underlying GP model
+
+    def __init__(
+        self,
+        kernel: Kernel,
+        n_modes: tuple[int, int] = (101, 101),
+        coefficients: Array | None = None,
+    ):
+        self.gp = FourierGP(n_modes=n_modes, kernel=kernel, coefficients=coefficients)
+
+    def __call__(self, data: SpatialData) -> Array:
+        return self.gp(data)
+
+
+class PositiveGPField(SpatialModel):
+    """A spatial field model represented by an underlying Gaussian Process constrained to be positive with a zero or tiny lower bound. Uses the softplus transformation to ensure positivity."""
+
+    gp: FourierGP  # The underlying GP model
+
+    def __init__(
+        self,
+        kernel: Kernel,
+        n_modes: tuple[int, int] = (101, 101),
+        coefficients: Array | None = None,
+        lower: float = 0.0,
+    ):
+        if lower < 0.0:
+            raise ValueError("Lower bound for PositiveGPField must be non-negative.")
+
+        self.gp = FourierGP(n_modes=n_modes, kernel=kernel, coefficients=coefficients)
+        self.lower = lower
+
+    def __call__(self, data: SpatialData) -> Array:
+        return l_bounded(self.gp(data), lower=self.lower)
+
+
+class LogGPField(SpatialModel):
+    """A spatial field model represented by a Log-Gaussian Process, i.e., the exponentiation of an underlying Gaussian Process."""
+
+    gp: FourierGP  # The underlying GP model
+
+    def __init__(
+        self,
+        kernel: Kernel,
+        n_modes: tuple[int, int] = (101, 101),
+        coefficients: Array | None = None,
+    ):
+        self.gp = FourierGP(n_modes=n_modes, kernel=kernel, coefficients=coefficients)
+
+    def __call__(self, data: SpatialData) -> Array:
+        return jnp.exp(self.gp(data))
+
+
+class FieldFromRatio(SpatialModel):
+    """A spatial field model represented as the product of a base field and a ratio field."""
+
+    # Brain hurt. This is represented_field = ratio_field * base_field. In galaxy studies, usually the denominator of the ratio is the stronger signal, e.g. H-alpha. So in this way we can model the ratio field directly and multiply by the base (strong line) field to ge the the weaker line field.
+
+    base_field: SpatialModel
+    ratio_field: SpatialModel
+
+    def __init__(
+        self,
+        base_field: SpatialModel,
+        ratio_field: SpatialModel,
+    ):
+        self.base_field = base_field
+        self.ratio_field = ratio_field
+
+    def __call__(self, data: SpatialData) -> Array:
+        return self.base_field(data) * self.ratio_field(data)
+
+
+class FieldPlusScatter(SpatialModel):
+    """A spatial field model represented as a base field model and a per-spaxel scatter term. Useful for having a mostly continuous component with some sparse scatter that is not well modelled by the continuous field."""
+
+    base_field: SpatialModel
+    scatter: PerSpaxel
+
+    def __init__(
+        self,
+        base_field: SpatialModel,
+        scatter: PerSpaxel,
+    ):
+        self.base_field = base_field
+        self.scatter = scatter
+
+    def __call__(self, data: SpatialData) -> Array:
+        return self.base_field(data) + self.scatter(data)
