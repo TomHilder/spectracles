813596742f650703e405666a143767c270f0250a
Tom Hilder <tom.hilder.dlhp@gmail.com>
2026-03-18 16:53:07 +1100

    line ratio model for blended doublet as stronger line

--- src/spectracles/lvm_models/ratios.py
+++ src/spectracles/lvm_models/ratios.py
@@ -17,7 +17,7 @@ from .. import (
     l_bounded,
 )
 from ..model.spatial import PerIFU, PerTile, PerTilePinned, SpatialModel
-from .fields import FieldFromRatio, GPField, PositiveGPField
+from .fields import FieldFromRatio, GPField, PositiveGPField, SumTwoFields
 from .likelihood import ln_likelihood
 from .line_single import EmissionLineVCal, WaveCalVelocity
 
@@ -39,7 +39,7 @@ class FluxCalFactor(SpatialModel):
 
 
 class LineRatioModel(SpectralSpatialModel):
-    """Model of two linked emission lines where the flux spatial field of the weak line is represented via a line ratio field multiplied by the flux spatial field of the strong line. line_1 is the strong line (denominator), line_2 is the weak line (numerator)."""
+    """Model of two emission lines where the flux spatial field of the weak line is represented via a line ratio field multiplied by the flux spatial field of the strong line. line_1 is the strong line (denominator), line_2 is the weak line (numerator)."""
 
     # Emission lines
     line_1: EmissionLineVCal
@@ -87,7 +87,8 @@ class LineRatioModel(SpectralSpatialModel):
         # Systematics / calibration corrections
         v_cal_1 = WaveCalVelocity(C_v_cal=C_v_cal_1, μ=μ_1)
         if share_v_cal:
-            v_cal_2 = v_cal_1
+            # v_cal_2 = v_cal_1
+            v_cal_2 = WaveCalVelocity(C_v_cal=C_v_cal_1, μ=μ_2)
         else:
             v_cal_2 = WaveCalVelocity(C_v_cal=C_v_cal_2, μ=μ_2)
         # Emission lines
@@ -171,7 +172,152 @@ class LineRatioModel(SpectralSpatialModel):
         return fcal_1 * comp_1 + fcal_2 * comp_2
 
 
-def neg_ln_posterior(model, λ, xy_data, data, u_data, mask, shared_kinematics=True):
+class DoubleLineRatioModel(SpectralSpatialModel):
+    """Mode of three emission lines. Two, stronger lines, with a total flux spatial field. The single, weaker line, is represented via a line ratio field multiplied by the total flux spatial field of the stronger lines. lines_1 are the total contributions from the stronger lines (denominator) and line_2 is the weaker line (numerator). TODO: update"""
+
+    # Emission lines
+    s_line_1: EmissionLineVCal
+    s_line_2: EmissionLineVCal
+    w_line: EmissionLineVCal
+
+    # Continuum level
+    cont_s: WindowConstant
+    cont_w: WindowConstant
+
+    # Calibration/Nuisances
+    flux_cal_s: FluxCalFactor
+    flux_cal_w: FluxCalFactor
+
+    def __init__(
+        self,
+        n_tiles: int,
+        n_spaxels: int,
+        n_modes: tuple[int, int],
+        μ_s1: AnyParameter,
+        μ_s2: AnyParameter,
+        μ_w: AnyParameter,
+        v_bary: AnyParameter,
+        v_syst_s: AnyParameter,
+        v_syst_w: AnyParameter,
+        σ_lsf_s1: AnyParameter,
+        σ_lsf_s2: AnyParameter,
+        σ_lsf_w: AnyParameter,
+        A_kernel: Kernel,
+        v_kernel: Kernel,
+        vσ_kernel: Kernel,
+        r_kernel: Kernel,
+        r_mean_log10: AnyParameter,
+        line_s_λ_window: tuple[float, float],
+        line_w_λ_window: tuple[float, float],
+        C_v_cal_s: AnyParameter,  # MUST be 2 values i.e. shape is (2,)
+        C_v_cal_w: AnyParameter,  # MUST be 2 values i.e. shape is (2,)
+        share_f_cal: bool = False,
+        share_v_cal: bool = False,
+        share_kinematics: bool = True,
+    ):
+        # Barycentric correction and LSFs as per-spaxel sub-models
+        # Most like Knowns but generically parameters
+        v_bary_ = PerSpaxel(n_spaxels=n_spaxels, spaxel_values=v_bary)
+        σ_lsf_s1_ = PerSpaxel(n_spaxels=n_spaxels, spaxel_values=σ_lsf_s1)
+        σ_lsf_s2_ = PerSpaxel(n_spaxels=n_spaxels, spaxel_values=σ_lsf_s2)
+        σ_lsf_w_ = PerSpaxel(n_spaxels=n_spaxels, spaxel_values=σ_lsf_w)
+        # Systematics / calibration corrections
+        v_cal_s1 = WaveCalVelocity(C_v_cal=C_v_cal_s, μ=μ_s1)
+        v_cal_s2 = WaveCalVelocity(C_v_cal=C_v_cal_s, μ=μ_s2)
+        if share_v_cal:
+            v_cal_w = WaveCalVelocity(C_v_cal=C_v_cal_s, μ=μ_w)
+        else:
+            v_cal_w = WaveCalVelocity(C_v_cal=C_v_cal_w, μ=μ_w)
+        # Emission lines
+        self.line_s1 = EmissionLineVCal(
+            μ=μ_s1,
+            A=PositiveGPField(kernel=A_kernel, n_modes=n_modes),
+            v=GPField(kernel=v_kernel, n_modes=n_modes),
+            vσ=PositiveGPField(kernel=vσ_kernel, n_modes=n_modes),
+            σ_lsf=σ_lsf_s1_,
+            v_bary=v_bary_,
+            v_syst=v_syst_s,
+            v_cal=v_cal_s1,
+        )
+        self.line_s2 = EmissionLineVCal(
+            μ=μ_s2,
+            A=PositiveGPField(kernel=A_kernel, n_modes=n_modes),
+            v=self.line_s1.v,
+            vσ=self.line_s1.vσ,
+            σ_lsf=σ_lsf_s2_,
+            v_bary=v_bary_,
+            v_syst=v_syst_s,
+            v_cal=v_cal_s2,
+        )
+        # Are the kinematics coupled, or independent?
+        if share_kinematics:
+            lw_v = self.line_s1.v
+            lw_vσ = self.line_s1.vσ
+        else:
+            lw_v = GPField(kernel=v_kernel, n_modes=n_modes)
+            lw_vσ = PositiveGPField(kernel=v_kernel, n_modes=n_modes)
+        # Weak line setup with intensity given by a ratio to total of strong lines fluxes
+        self.line_w = EmissionLineVCal(
+            μ=μ_w,
+            A=FieldFromRatio(
+                base_field=SumTwoFields(self.line_s1.A, self.line_s2.A),
+                log10_ratio_field=GPField(kernel=r_kernel, n_modes=n_modes),
+                log10_ratio_mean=r_mean_log10,
+            ),
+            v=lw_v,
+            vσ=lw_vσ,
+            σ_lsf=σ_lsf_w_,
+            v_syst=v_syst_w,
+            v_cal=v_cal_w,
+        )
+        # Local continuum to each line
+        self.cont_s = WindowConstant(
+            const=PerSpaxel(n_spaxels=n_spaxels),
+            λ_min=line_s_λ_window[0],
+            λ_max=line_s_λ_window[1],
+        )
+        self.cont_2 = WindowConstant(
+            const=PerSpaxel(n_spaxels=n_spaxels),
+            λ_min=line_w_λ_window[0],
+            λ_max=line_w_λ_window[1],
+        )
+        # Flux cal
+        self.flux_cal_s = FluxCalFactor(
+            n_tiles=n_tiles,
+            tile_values=Parameter(jnp.zeros((n_tiles,)), fixed=True),
+            ifu_values=Parameter(jnp.zeros((3,)), fixed=True),
+        )
+        if share_f_cal:
+            self.flux_cal_w = self.flux_cal_s
+        else:
+            self.flux_cal_w = FluxCalFactor(
+                n_tiles=n_tiles,
+                tile_values=Parameter(jnp.zeros((n_tiles,)), fixed=True),
+                ifu_values=Parameter(jnp.zeros((3,)), fixed=True),
+            )
+        # Convenience function
+
+    def log10_ratio(self, spatial_data: SpatialDataLVM):
+        return self.line_w.A.log10_ratio(spatial_data)
+
+    def __call__(self, λ: Array, spatial_data: SpatialDataLVM) -> tuple[Array, Array]:
+        """Return the model flux for both lines at the given wavelengths and spatial data."""
+        comp_s = self.line_s(λ, spatial_data) + self.cont_s(λ, spatial_data)
+        comp_w = self.line_w(λ, spatial_data) + self.cont_w(λ, spatial_data)
+        fcal_s = self.flux_cal_s(spatial_data)
+        fcal_w = self.flux_cal_w(spatial_data)
+        return fcal_s * comp_s + fcal_w * comp_w
+
+
+def neg_ln_posterior(
+    model,
+    λ,
+    xy_data,
+    data,
+    u_data,
+    mask,
+    shared_kinematics=True,
+):
     vmapped_model = jax.vmap(model, in_axes=(0, None))
     ln_like = ln_likelihood(vmapped_model, λ, xy_data, data, u_data, mask)
     locked_model = model.get_locked_model()
@@ -195,12 +341,40 @@ def neg_ln_posterior(model, λ, xy_data, data, u_data, mask, shared_kinematics=T
         ln_prior
         + norm.logpdf(x=locked_model.flux_cal_2.f_cal_raw.tile_values.val, loc=0, scale=0.1).sum()
     )
-    # ln_prior = (
-    #     locked_model.line_1.A.gp.prior_logpdf()
-    #     + locked_model.line_1.v.gp.prior_logpdf()
-    #     + locked_model.line_1.vσ.gp.prior_logpdf()
-    #     + locked_model.line_2.A.log10_ratio_field.gp.prior_logpdf()
-    #     + norm.logpdf(x=locked_model.flux_cal_1.f_cal_raw.tile_values.val, loc=0, scale=0.1).sum()
-    #     + norm.logpdf(x=locked_model.flux_cal_2.f_cal_raw.tile_values.val, loc=0, scale=0.1).sum()
-    # )
+    return -1 * (ln_like + ln_prior)
+
+
+def neg_ln_posterior_double(
+    model,
+    λ,
+    xy_data,
+    data,
+    u_data,
+    mask,
+    shared_kinematics=True,
+):
+    vmapped_model = jax.vmap(model, in_axes=(0, None))
+    ln_like = ln_likelihood(vmapped_model, λ, xy_data, data, u_data, mask)
+    locked_model = model.get_locked_model()
+    # === Get the prior
+    ln_prior = 0
+    # Strong line contributions
+    ln_prior = ln_prior + locked_model.line_s1.A.gp.prior_logpdf()
+    ln_prior = ln_prior + locked_model.line_s2.A.gp.prior_logpdf()
+    ln_prior = ln_prior + locked_model.line_s1.v.gp.prior_logpdf()
+    ln_prior = ln_prior + locked_model.line_s1.vσ.gp.prior_logpdf()
+    # Weak line contributions
+    ln_prior = ln_prior + locked_model.line_w.A.log10_ratio_field.gp.prior_logpdf()
+    if not shared_kinematics:
+        ln_prior = ln_prior + locked_model.line_w.v.gp.prior_logpdf()
+        ln_prior = ln_prior + locked_model.line_w.vσ.gp.prior_logpdf()
+    # Other priors
+    ln_prior = (
+        ln_prior
+        + norm.logpdf(x=locked_model.flux_cal_1.f_cal_raw.tile_values.val, loc=0, scale=0.1).sum()
+    )
+    ln_prior = (
+        ln_prior
+        + norm.logpdf(x=locked_model.flux_cal_2.f_cal_raw.tile_values.val, loc=0, scale=0.1).sum()
+    )
     return -1 * (ln_like + ln_prior)
